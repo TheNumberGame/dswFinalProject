efbe5d2ddabd8f8ca13c3a6dbd1a0e6f14de0912
TheNumberGame <31251497+TheNumberGame@users.noreply.github.com>
2018-04-05 11:08:15 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -37,37 +37,37 @@ github = oauth.remote_app(
     authorize_url='https://github.com/login/oauth/authorize' #URL for github's OAuth login
 )
 
-@app.context_processor
-def inject_logged_in():
-    return {"logged_in":('github_token' in session)}
+#@app.context_processor
+#def inject_logged_in():
+#    return {"logged_in":('github_token' in session)}
 
-@app.route('/login')
-def login():   
-    return github.authorize(callback=url_for('authorized', _external=True, _scheme='https')) #callback URL must match the pre-configured callback URL
+#@app.route('/login')
+#def login():   
+#    return github.authorize(callback=url_for('authorized', _external=True, _scheme='https')) #callback URL must match the pre-configured callback URL
 
 #Start here
 
-@app.route('/logout')
-def logout():
-    session.clear()
-    return render_template('message.html', message='You were logged out')
+#@app.route('/logout')
+#def logout():
+#    session.clear()
+#    return render_template('message.html', message='You were logged out')
 
-@app.route('/login/authorized')
-def authorized():
-    resp = github.authorized_response()
-    if resp is None:
-        session.clear()
-        message = 'Access denied: reason=' + request.args['error'] + ' error=' + request.args['error_description'] + ' full=' + pprint.pformat(request.args)      
-    else:
-        try:
-            session['github_token'] = (resp['access_token'], '') #save the token to prove that the user logged in
-            session['user_data']=github.get('user').data
-            message='You were successfully logged in as ' + session['user_data']['login']
-        except Exception as inst:
-            session.clear()
-            print(inst)
-            message='Unable to login, please try again.  '
-    return render_template('message.html', message=message)
+#@app.route('/login/authorized')
+#def authorized():
+#    resp = github.authorized_response()
+#    if resp is None:
+#        session.clear()
+#        message = 'Access denied: reason=' + request.args['error'] + ' error=' + request.args['error_description'] + ' full=' + pprint.pformat(request.args)      
+#    else:
+#        try:
+#            session['github_token'] = (resp['access_token'], '') #save the token to prove that the user logged in
+#            session['user_data']=github.get('user').data
+#            message='You were successfully logged in as ' + session['user_data']['login']
+#        except Exception as inst:
+#            session.clear()
+#            print(inst)
+#            message='Unable to login, please try again.  '
+#    return render_template('message.html', message=message)
 
 if __name__ == '__main__':
     app.run()
