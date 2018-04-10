824a23b616c0176c764111c01ea50970277c57b9
TheNumberGame <31251497+TheNumberGame@users.noreply.github.com>
2018-04-10 11:29:45 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -59,7 +59,7 @@ def post():
         temp_file_id = fs.put(request.files['file'])
     else:
         temp_file_id = None
-    
+    print("Working?")
     if not request.form['message'] == "" and not request.form['message'].isspace():
         if not temp_file_id == None:
              data = { "_id": ObjectId(), "pic_id": temp_file_id, "name": session['user_data']['login'], "message": escape(request.form['message']), "date": str(datetime.now())}
