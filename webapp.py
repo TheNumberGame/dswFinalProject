552ad751bb34dabd2a16a7b4de2e5cc048aae41d
TheNumberGame <31251497+TheNumberGame@users.noreply.github.com>
2018-04-10 16:54:58 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -117,7 +117,7 @@ def delPost():
 
 @app.route("/img/<filename>")
 def post_img(filename):
-     image = fs.get_latest_version(filename=filename)
+     image = fs.find_one(filename=filename)
      response.content_type = 'image/'+ filename.split('.')[1]
      return image
 
