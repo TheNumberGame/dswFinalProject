9cfc22c9112a60aade422f0daa1032a09744bfe6
TheNumberGame <31251497+TheNumberGame@users.noreply.github.com>
2018-04-10 14:41:55 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -81,7 +81,7 @@ def posts_to_html(data = None):
           try:
                for i in data.sort([("date", -1)]):
                     if not i['pic_id'] == 0:
-                         option += Markup("<img src=\"\" alt=\"picture\" height=\"50\" width=\"50\">")
+                         option += Markup("<img src=\"/img/"+ fs.get(i['pic_id']).filename+"\" alt=\"picture\" height=\"50\" width=\"50\">")
                     option += Markup("<p class=\"mes\" ><span style=\"color:blue;\">" + i["name"] + "</span>: " + i["message"]) 
                     if i['name'] == session['user_data']['login']:
                          option += Markup("<br><button type=\"submit\" name=\"DeletePost\" value= \""+ str(i["_id"]) +"\">Delete Post</button>  <span style=\"color:green;\">Date Posted</span>: "+ str(i["date"]) +"</p>")
@@ -105,6 +105,11 @@ def delPost():
    
     return redirect(url_for("home"))
 
+@app.route("/img/<filename>")
+def post_img(filename):
+     image = fs.get_latest_version(filename=filename)
+     
+     return image
 
 @app.route('/logout')
 def logout():
