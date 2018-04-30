4efbb4f10698c3ebc6e957b073fdf670e0223fe8
TheNumberGame <31251497+TheNumberGame@users.noreply.github.com>
2018-04-30 13:37:36 -0700

    Update socialHour.js

--- static/socialHour.js
+++ static/socialHour.js
@@ -1,6 +1,6 @@
 $(document).ready( function(){
      $(".toTextBox").click( function(){
-          var textBox = "<textarea name=\"message\" form=\"replyForm\" style=\"width:100%; height:100px;\"></textarea><br><input name=\"file\" type=\"file\" form=\"replyForm\"><br><input type=\"submit\" value=\"Post\">"
+          var textBox = "<textarea name=\"message\" form=\"#replyForm\" style=\"width:100%; height:100px;\"></textarea><br><input name=\"file\" type=\"file\" form=\"#replyForm\"><br><input type=\"submit\" value=\"Post\">"
           $(this).replaceWith(textBox);
      });
 });
