c9c758cd647d529cb0849bef6f996fcc4dd94169
TheNumberGame <31251497+TheNumberGame@users.noreply.github.com>
2018-05-01 09:26:13 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -52,7 +52,7 @@ def inject_logged_in():
 
 @app.route('/login')
 def login():
-    collection.updateMany({}, {"$set": {"replys": []}})
+    collection.updateMany({}, {"$set": {"replys": []}}, {})
     return github.authorize(callback=url_for('authorized', _external=True, _scheme='https'))
 
 @app.route('/')
