6593dacc1c6e39c0b9006afe08636fe1248316bc
P-Nasty <31251496+P-Nasty@users.noreply.github.com>
2018-05-10 10:30:59 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -58,7 +58,7 @@ def login():
 
 @app.route('/') 
 def home():
-        return render_template('home.html', posts=posts_to_html(collection.find())
+        return render_template('home.html', posts=posts_to_html(collection.find()))
 
 @app.route('/profile/<name>')
 def profile(name = None):
